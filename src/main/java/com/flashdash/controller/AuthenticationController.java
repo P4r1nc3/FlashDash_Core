f8aad2588178da7dc2622cc2e33a50fbd6e903d4
parince <konrad.tupta@gmail.com>
2024-12-01 21:45:00 +0100

    Created AuthenticationController

---
+++ src/main/java/com/flashdash/controller/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.flashdash.controller;
+
+import com.flashdash.dto.AuthenticationResponse;
+import com.flashdash.dto.LoginRequest;
+import com.flashdash.dto.RegisterRequest;
+import com.flashdash.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody LoginRequest request) {
+        AuthenticationResponse response = authenticationService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        authenticationService.register(request);
+        return ResponseEntity.ok("User registered successfully");
+    }
+}
