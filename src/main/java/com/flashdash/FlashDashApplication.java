442de0a114918e89526ce388507dd018f380eaea
parince <konrad.tupta@gmail.com>
2024-12-01 14:46:57 +0100

    Initial spring boot configuration

---
+++ src/main/java/com/flashdash/FlashDashApplication.java
@@ -0,0 +1,13 @@
+package com.flashdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashDashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashDashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flashdash/FlashDashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashDashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
