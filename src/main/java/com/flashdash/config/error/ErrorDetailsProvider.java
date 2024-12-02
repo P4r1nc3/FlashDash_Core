58420c0a8756ef81e6afab3c5a81b45582b4cee7
parince <konrad.tupta@gmail.com>
2024-12-02 16:42:58 +0100

    Adjusted ErrorDetailsProvider

--- src/main/java/com/flashdash/config/error/ErrorDetailsProvider.java
+++ src/main/java/com/flashdash/config/error/ErrorDetailsProvider.java
@@ -1,13 +1,17 @@
 package com.flashdash.config.error;
 
+import com.flashdash.dto.ErrorResponse;
+import org.slf4j.MDC;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.Locale;
 
 @Component
 public class ErrorDetailsProvider {
 
+    private static final String CORRELATION_ID_KEY = "correlationId";
     private final ResourceBundleMessageSource messageSource;
 
     public ErrorDetailsProvider() {
@@ -16,15 +20,26 @@ public class ErrorDetailsProvider {
         this.messageSource.setDefaultEncoding("UTF-8");
     }
 
-    public int getStatus(ErrorCode errorCode) {
+    public ErrorResponse createErrorResponse(ErrorCode errorCode) {
+        return new ErrorResponse(
+                getStatus(errorCode),
+                errorCode.name(),
+                getCause(errorCode),
+                getAction(errorCode),
+                LocalDateTime.now(),
+                MDC.get(CORRELATION_ID_KEY)
+        );
+    }
+
+    private int getStatus(ErrorCode errorCode) {
         return Integer.parseInt(getMessage(errorCode.name() + ".status"));
     }
 
-    public String getCause(ErrorCode errorCode) {
+    private String getCause(ErrorCode errorCode) {
         return getMessage(errorCode.name() + ".cause");
     }
 
-    public String getAction(ErrorCode errorCode) {
+    private String getAction(ErrorCode errorCode) {
         return getMessage(errorCode.name() + ".action");
     }
 
--- src/main/java/com/flashdash/config/error/GlobalExceptionHandler.java
+++ src/main/java/com/flashdash/config/error/GlobalExceptionHandler.java
@@ -1,19 +1,14 @@
 package com.flashdash.config.error;
 
 import com.flashdash.dto.ErrorResponse;
-import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     private final ErrorDetailsProvider errorDetailsProvider;
-
-    private static final String CORRELATION_ID_KEY = "correlationId";
 
     public GlobalExceptionHandler(ErrorDetailsProvider errorDetailsProvider) {
         this.errorDetailsProvider = errorDetailsProvider;
@@ -21,18 +16,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(FlashDashException.class)
     public ResponseEntity<ErrorResponse> handleFlashDashException(FlashDashException ex) {
-        String correlationId = MDC.get(CORRELATION_ID_KEY);
-        var errorCode = ex.getErrorCode();
-
-        ErrorResponse errorResponse = new ErrorResponse(
-                errorDetailsProvider.getStatus(errorCode),
-                errorCode.name(),
-                errorDetailsProvider.getCause(errorCode),
-                errorDetailsProvider.getAction(errorCode),
-                LocalDateTime.now(),
-                correlationId
-        );
-
+        ErrorResponse errorResponse = errorDetailsProvider.createErrorResponse(ex.getErrorCode());
         return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
     }
 }
