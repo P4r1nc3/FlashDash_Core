d1afb3a31d02b00786af4671f7d112485aaf0b91
P4r1nc3 <konrad.tupta@gmail.com>
2024-12-02 18:39:36 +0100

    Adjusted User model

--- src/main/java/com/flashdash/model/User.java
+++ src/main/java/com/flashdash/model/User.java
@@ -42,14 +42,38 @@ public class User implements UserDetails {
         return Collections.emptyList();
     }
 
-    @Override
-    public String getPassword() {
-        return password;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     @Override
     public String getUsername() {
         return email;
+    }
+
+    public void setUsername(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
