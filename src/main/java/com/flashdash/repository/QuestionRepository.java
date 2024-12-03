4fa941a4e729611a3d441e176d17e96c2ac8083e
P4r1nc3 <konrad.tupta@gmail.com>
2024-12-03 10:09:38 +0100

    Created QuestionRepository

---
+++ src/main/java/com/flashdash/repository/QuestionRepository.java
@@ -0,0 +1,14 @@
+package com.flashdash.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.flashdash.model.Deck;
+import com.flashdash.model.question.Question;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findAllByDeck(Deck deck);
+    Optional<Question> findByDeckAndQuestionId(Deck deck, Long cardId);
+    void deleteAllByDeck(Deck deck);
+}
