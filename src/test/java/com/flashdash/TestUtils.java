0b0f23d5e22323efd1848325ba0ffd959e2f59a5
P4r1nc3 <konrad.tupta@gmail.com>
2024-12-03 11:05:12 +0100

    Created CardRepositoryTest

--- src/test/java/com/flashdash/TestUtils.java
+++ src/test/java/com/flashdash/TestUtils.java
@@ -6,8 +6,11 @@ import com.flashdash.dto.RegisterRequest;
 import com.flashdash.model.User;
 import com.flashdash.model.question.Question;
 import com.flashdash.model.question.QuestionDeck;
+import com.flashdash.model.card.Card;
+import com.flashdash.model.card.CardDeck;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
 
 public class TestUtils {
@@ -50,7 +53,7 @@ public class TestUtils {
         deck.setCreatedAt(LocalDateTime.now());
         deck.setUpdatedAt(LocalDateTime.now());
         deck.setUser(user);
-        deck.setQuestions(new java.util.HashSet<>());
+        deck.setQuestions(new HashSet<>());
         return deck;
     }
 
@@ -64,5 +67,26 @@ public class TestUtils {
         question.setUpdatedAt(LocalDateTime.now());
         question.setDeck(deck);
         return question;
+    }
+    public static CardDeck createCardDeck(User user) {
+        CardDeck deck = new CardDeck();
+        deck.setName("Sample Card Deck");
+        deck.setDescription("This is a sample card deck.");
+        deck.setCreatedAt(LocalDateTime.now());
+        deck.setUpdatedAt(LocalDateTime.now());
+        deck.setUser(user);
+        deck.setCards(new HashSet<>());
+        return deck;
+    }
+
+    public static Card createCard(CardDeck deck, String question, String answer) {
+        Card card = new Card();
+        card.setQuestion(question);
+        card.setAnswer(answer);
+        card.setDifficulty("Medium");
+        card.setCreatedAt(LocalDateTime.now());
+        card.setUpdatedAt(LocalDateTime.now());
+        card.setDeck(deck);
+        return card;
     }
 }
