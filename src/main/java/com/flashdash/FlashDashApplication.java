44ea56844c4d357c923c5077be0ba921e085abc7
P4r1nc3 <konrad.tupta@gmail.com>
2024-12-03 13:03:26 +0100

    Fixed FlashDashApplication.java

--- src/main/java/com/flashdash/FlashDashApplication.java
+++ src/main/java/com/flashdash/FlashDashApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FlashDashApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlashDashApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FlashDashApplication.class, args);
+    }
 
 }
