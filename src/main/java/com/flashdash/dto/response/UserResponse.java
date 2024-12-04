ca63137e511bf3b522e45282016d36e81c76b512
P4r1nc3 <konrad.tupta@gmail.com>
2024-12-04 10:53:18 +0100

    Adjusted UserResponse

--- src/main/java/com/flashdash/dto/response/UserResponse.java
+++ src/main/java/com/flashdash/dto/response/UserResponse.java
@@ -1,10 +1,20 @@
 package com.flashdash.dto.response;
 
+import java.time.LocalDateTime;
+
 public class UserResponse {
 
     private String firstName;
     private String lastName;
     private String email;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    public UserResponse() {
+
+    }
 
     public UserResponse(String firstName, String lastName, String email) {
         this.firstName = firstName;
@@ -34,5 +44,21 @@ public class UserResponse {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
