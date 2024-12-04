2a6b0637874ad0783001f287e2b54c2951021771
P4r1nc3 <konrad.tupta@gmail.com>
2024-12-04 10:56:28 +0100

    Added tests for getCurrentUser

--- src/test/java/com/flashdash/service/UserServiceTest.java
+++ src/test/java/com/flashdash/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.flashdash.service;
 
 import com.flashdash.FlashDashApplication;
 import com.flashdash.TestUtils;
+import com.flashdash.dto.response.UserResponse;
 import com.flashdash.exception.FlashDashException;
 import com.flashdash.exception.ErrorCode;
 import com.flashdash.model.User;
@@ -27,6 +28,39 @@ class UserServiceTest {
 
     @MockBean
     private UserRepository userRepository;
+
+    @Test
+    void shouldReturnCurrentUserSuccessfully() {
+        // Arrange
+        User user = TestUtils.createUser();
+        when(userRepository.findByEmail("test@example.com")).thenReturn(Optional.of(user));
+
+        // Act
+        UserResponse userResponse = userService.getCurrentUser("test@example.com");
+
+        // Assert
+        assertThat(userResponse).isNotNull();
+        assertThat(userResponse.getFirstName()).isEqualTo(user.getFirstName());
+        assertThat(userResponse.getLastName()).isEqualTo(user.getLastName());
+        assertThat(userResponse.getEmail()).isEqualTo(user.getUsername());
+        assertThat(userResponse.getCreatedAt()).isEqualTo(user.getCreatedAt());
+        assertThat(userResponse.getUpdatedAt()).isEqualTo(user.getUpdatedAt());
+
+        verify(userRepository).findByEmail("test@example.com");
+    }
+
+    @Test
+    void shouldThrowExceptionWhenCurrentUserNotFound() {
+        // Arrange
+        when(userRepository.findByEmail("nonexistent@example.com")).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThatThrownBy(() -> userService.getCurrentUser("nonexistent@example.com"))
+                .isInstanceOf(FlashDashException.class)
+                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.E404001);
+
+        verify(userRepository).findByEmail("nonexistent@example.com");
+    }
 
     @Test
     void shouldLoadUserByUsernameSuccessfully() {
