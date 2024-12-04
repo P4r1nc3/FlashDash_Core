e182e0adc909e831abda04bcb818fafbd0f40b19
P4r1nc3 <konrad.tupta@gmail.com>
2024-12-04 10:53:36 +0100

    Created getCurrentUser() method

--- src/main/java/com/flashdash/service/UserService.java
+++ src/main/java/com/flashdash/service/UserService.java
@@ -1,7 +1,9 @@
 package com.flashdash.service;
 
+import com.flashdash.dto.response.UserResponse;
 import com.flashdash.exception.ErrorCode;
 import com.flashdash.exception.FlashDashException;
+import com.flashdash.model.User;
 import com.flashdash.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,6 +17,23 @@ public class UserService implements UserDetailsService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public UserResponse getCurrentUser(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new FlashDashException(
+                        ErrorCode.E404001,
+                        "User with email " + email + " not found."
+                ));
+
+        UserResponse response = new UserResponse();
+        response.setFirstName(user.getFirstName());
+        response.setLastName(user.getLastName());
+        response.setEmail(user.getUsername());
+        response.setCreatedAt(user.getCreatedAt());
+        response.setUpdatedAt(user.getUpdatedAt());
+
+        return response;
     }
 
     @Override
