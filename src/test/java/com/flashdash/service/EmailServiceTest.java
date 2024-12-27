ac3c6833b86af4eb41d659835a2322c81c75a30a
P4r1nc3 <konrad.tupta@gmail.com>
2024-12-27 17:49:36 +0100

    Adjusted EmailServiceTest

--- src/test/java/com/flashdash/service/EmailServiceTest.java
+++ src/test/java/com/flashdash/service/EmailServiceTest.java
@@ -2,15 +2,16 @@ package com.flashdash.service;
 
 import com.flashdash.FlashDashApplication;
 import com.flashdash.TestUtils;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes = FlashDashApplication.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -23,40 +24,53 @@ class EmailServiceTest {
     private JavaMailSender mailSender;
 
     @Test
+    @SneakyThrows
     void testSendActivationEmail() {
         // Arrange
         String to = "test@example.com";
         String activationToken = "activationToken123";
+        SimpleMailMessage expectedMessage = TestUtils.createSimpleMailMessageForAccountActivation(to, activationToken);
+        doNothing().when(mailSender).send(expectedMessage);
 
         // Act
         emailService.sendActivationEmail(to, activationToken);
 
         // Assert
-        verify(mailSender, times(1)).send(TestUtils.createSimpleMailMessageForAccountActivation(to, activationToken));
+        Thread.sleep(10);
+        verify(mailSender, times(1)).send(expectedMessage);
     }
 
     @Test
+    @SneakyThrows
     void testSendDailyNotificationEmail() {
         // Arrange
         String to = "test@example.com";
+        SimpleMailMessage expectedMessage = TestUtils.createSimpleMailMessageForDailyNotification(to);
+        doNothing().when(mailSender).send(expectedMessage);
 
         // Act
         emailService.sendDailyNotificationEmail(to);
 
         // Assert
-        verify(mailSender, times(1)).send(TestUtils.createSimpleMailMessageForDailyNotification(to));
+        Thread.sleep(10);
+        verify(mailSender, times(1)).send(expectedMessage);
     }
 
     @Test
+    @SneakyThrows
     void testSendFriendInvitationEmail() {
         // Arrange
         String to = "test@example.com";
         String senderFirstName = "Konrad";
         String senderLastName = "Tupta";
+        SimpleMailMessage expectedMessage = TestUtils.createSimpleMailMessageForFriendInvitation(to, senderFirstName, senderLastName);
+        doNothing().when(mailSender).send(expectedMessage);
+
         // Act
         emailService.sendFriendInvitationEmail(to, senderFirstName, senderLastName);
 
         // Assert
-        verify(mailSender, times(1)).send(TestUtils.createSimpleMailMessageForFriendInvitation(to, senderFirstName, senderLastName));
+        Thread.sleep(10);
+        verify(mailSender, times(1)).send(expectedMessage);
     }
 }
