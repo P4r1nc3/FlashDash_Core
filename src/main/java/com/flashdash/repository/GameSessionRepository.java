bd180ebebe5007738b2447a4c9f51603da3d5ae8
P4r1nc3 <konrad.tupta@gmail.com>
2025-02-26 09:35:50 +0100

    Created findAllByUser method

--- src/main/java/com/flashdash/repository/GameSessionRepository.java
+++ src/main/java/com/flashdash/repository/GameSessionRepository.java
@@ -3,8 +3,11 @@ package com.flashdash.repository;
 import com.flashdash.model.GameSession;
 import com.flashdash.model.GameSessionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.flashdash.model.*;
+
+import java.util.List;
 
 public interface GameSessionRepository extends JpaRepository<GameSession, Long> {
-
     GameSession findTopByDeckIdAndUserIdAndStatus(Long deckId, Long userId, GameSessionStatus status);
+    List<GameSession> findAllByUser(User user);
 }
