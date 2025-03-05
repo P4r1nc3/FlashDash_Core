6d7a5751dd2f09830a8c3ca4bd6716908e5f83ea
parince <konrad.tupta@gmail.com>
2025-03-05 09:10:16 +0100

    Remade models

--- src/main/java/com/flashdash/model/User.java
+++ src/main/java/com/flashdash/model/User.java
@@ -1,108 +1,59 @@
 package com.flashdash.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "users")
 public class User implements UserDetails {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @Column(nullable = false)
+    @Id
+    @Column(name = "user_frn", nullable = false, length = 256)
+    private String userFrn;
+
+    @Column(name = "first_name", nullable = false, length = 256)
     private String firstName;
 
-    @Column(nullable = false)
+    @Column(name = "last_name", nullable = false, length = 256)
     private String lastName;
 
-    @Column(unique = true, nullable = false)
+    @Column(name = "email", unique = true, nullable = false, length = 256)
     private String email;
 
-    @Column(nullable = false)
+    @Column(name = "password", nullable = false, length = 256)
     private String password;
 
-    @Column(nullable = false, updatable = false)
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    @Column(nullable = false)
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
-    @Column(nullable = false)
-    private boolean enabled = false;
+    @Column(name = "enabled", nullable = false)
+    private boolean enabled;
 
-    @Column(unique = true)
+    @Column(name = "activation_token", unique = true, nullable = true, length = 256)
     private String activationToken;
 
-    @Column(nullable = false)
+    @Column(name = "daily_notifications", nullable = false)
     private boolean dailyNotifications;
 
-//    @OneToMany(mappedBy = "user")
-//    private Set<GameSession> gameSessions;
+    @ElementCollection
+    @CollectionTable(name = "user_friends", joinColumns = @JoinColumn(name = "user_frn"))
+    @Column(name = "friend_frn", length = 256)
+    private List<String> friendsFrn = new ArrayList<>();
 
-    @ManyToMany
-    @JoinTable(
-            name = "user_friends",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "friend_id")
-    )
-    private Set<User> friends = new HashSet<>();
-
-    @OneToMany(mappedBy = "sentTo", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<FriendInvitation> receivedInvitations = new HashSet<>();
-
-    @OneToMany(mappedBy = "sentBy", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<FriendInvitation> sentInvitations = new HashSet<>();
-
-    public User() {
-
-    }
-
-    public User(String firstName, String lastName, String email, String password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-    }
-
-    @PrePersist
-    protected void onCreate() {
-        createdAt = LocalDateTime.now();
-        updatedAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        updatedAt = LocalDateTime.now();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public User() {}
 
     @Override
     public String getUsername() {
@@ -116,74 +67,6 @@ public class User implements UserDetails {
     @Override
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public String getActivationToken() {
-        return activationToken;
-    }
-
-    public void setActivationToken(String activationToken) {
-        this.activationToken = activationToken;
-    }
-
-    public boolean isDailyNotifications() {
-        return dailyNotifications;
-    }
-
-    public void setDailyNotifications(boolean dailyNotifications) {
-        this.dailyNotifications = dailyNotifications;
-    }
-
-//    public Set<GameSession> getGameSessions() {
-//        return gameSessions;
-//    }
-//
-//    public void setGameSessions(Set<GameSession> gameSessions) {
-//        this.gameSessions = gameSessions;
-//    }
-
-    public Set<User> getFriends() {
-        return friends;
-    }
-
-    public void setFriends(Set<User> friends) {
-        this.friends = friends;
-    }
-
-    public Set<FriendInvitation> getReceivedInvitations() {
-        return receivedInvitations;
-    }
-
-    public void setReceivedInvitations(Set<FriendInvitation> receivedInvitations) {
-        this.receivedInvitations = receivedInvitations;
-    }
-
-    public Set<FriendInvitation> getSentInvitations() {
-        return sentInvitations;
-    }
-
-    public void setSentInvitations(Set<FriendInvitation> sentInvitations) {
-        this.sentInvitations = sentInvitations;
     }
 
     @Override
