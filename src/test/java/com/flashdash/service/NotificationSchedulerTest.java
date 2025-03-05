6f32166b49d1461df0c6e1a07a51b22ed37a8c7d
P4r1nc3 <konrad.tupta@gmail.com>
2025-03-05 17:28:36 +0100

    Adjusted NotificationSchedulerTest

--- src/test/java/com/flashdash/service/NotificationSchedulerTest.java
+++ src/test/java/com/flashdash/service/NotificationSchedulerTest.java
@@ -4,13 +4,13 @@ import com.flashdash.FlashDashApplication;
 import com.flashdash.TestUtils;
 import com.flashdash.model.User;
 import com.flashdash.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
@@ -18,6 +18,7 @@ import static org.mockito.Mockito.*;
 @SpringBootTest(classes = FlashDashApplication.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class NotificationSchedulerTest {
+
     @Autowired
     private NotificationScheduler notificationScheduler;
 
@@ -27,68 +28,70 @@ class NotificationSchedulerTest {
     @MockitoBean
     private EmailService emailService;
 
+    private User user1;
+    private User user2;
+
+    @BeforeEach
+    void setUp() {
+        user1 = TestUtils.createUser();
+        user1.setDailyNotifications(true);
+
+        user2 = TestUtils.createUser();
+        user2.setDailyNotifications(true);
+    }
+
     @Test
     void shouldSendDailyNotificationsToUsersWithNotificationsEnabled() {
         // Arrange
-        User user1 = TestUtils.createUser();
-        user1.setDailyNotifications(true);
-
-        User user2 = TestUtils.createFriendUser();
-        user2.setDailyNotifications(true);
-
-        // Only return users with dailyNotifications set to true
-        List<User> users = Arrays.asList(user1, user2);
-
+        List<User> users = List.of(user1, user2);
         when(userRepository.findByDailyNotificationsTrue()).thenReturn(users);
 
         // Act
         notificationScheduler.sendDailyNotifications();
 
         // Assert
-        verify(emailService, times(1)).sendDailyNotificationEmail(user1.getUsername());
-        verify(emailService, times(1)).sendDailyNotificationEmail(user2.getUsername());
+        verify(emailService, times(1)).sendDailyNotificationEmail(user1.getEmail());
+        verify(emailService, times(1)).sendDailyNotificationEmail(user2.getEmail());
     }
 
     @Test
     void shouldNotSendNotificationsToUsersWithoutNotificationsEnabled() {
         // Arrange
-        User user1 = TestUtils.createUser();
-        user1.setDailyNotifications(true);
-
-        User user2 = TestUtils.createFriendUser();
+        user1.setDailyNotifications(false);
         user2.setDailyNotifications(false);
-
-        List<User> users = Arrays.asList(user1);
-
-        when(userRepository.findByDailyNotificationsTrue()).thenReturn(users);
+        when(userRepository.findByDailyNotificationsTrue()).thenReturn(List.of());
 
         // Act
         notificationScheduler.sendDailyNotifications();
 
         // Assert
-        verify(emailService, times(1)).sendDailyNotificationEmail(user1.getUsername());
-        verify(emailService, times(0)).sendDailyNotificationEmail(user2.getUsername());
+        verify(emailService, never()).sendDailyNotificationEmail(anyString());
+    }
+
+    @Test
+    void shouldSendNotificationOnlyToUsersWithEnabledNotifications() {
+        // Arrange
+        user1.setDailyNotifications(true);
+        user2.setDailyNotifications(false);
+        when(userRepository.findByDailyNotificationsTrue()).thenReturn(List.of(user1));
+
+        // Act
+        notificationScheduler.sendDailyNotifications();
+
+        // Assert
+        verify(emailService, times(1)).sendDailyNotificationEmail(user1.getEmail());
+        verify(emailService, never()).sendDailyNotificationEmail(user2.getEmail());
     }
 
     @Test
     void shouldNotSendNotificationsIfNoUsersHaveDailyNotificationsEnabled() {
         // Arrange
-        User user1 = TestUtils.createUser();
-        user1.setDailyNotifications(false);
-
-        User user2 = TestUtils.createFriendUser();
-        user2.setDailyNotifications(false);
-
-        List<User> users = Arrays.asList();
-
-        when(userRepository.findByDailyNotificationsTrue()).thenReturn(users);
+        when(userRepository.findByDailyNotificationsTrue()).thenReturn(List.of());
 
         // Act
         notificationScheduler.sendDailyNotifications();
 
         // Assert
-        verify(emailService, times(0)).sendDailyNotificationEmail(user1.getUsername());
-        verify(emailService, times(0)).sendDailyNotificationEmail(user2.getUsername());
+        verify(emailService, never()).sendDailyNotificationEmail(anyString());
     }
-
 }
