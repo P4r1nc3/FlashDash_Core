935a8a5e2cff4bea51d01fa0353fa83fcf62c297
parince <konrad.tupta@gmail.com>
2025-03-05 09:11:15 +0100

    Remade repositories

--- src/main/java/com/flashdash/repository/QuestionRepository.java
+++ src/main/java/com/flashdash/repository/QuestionRepository.java
@@ -1,20 +1,17 @@
 package com.flashdash.repository;
 
-import com.flashdash.model.Deck;
 import com.flashdash.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-    List<Question> findAllByDeck(Deck deck);
+public interface QuestionRepository extends JpaRepository<Question, String> {
+    List<Question> findAllByDeckFrn(String deckFrn);
 
-    Optional<Question> findByDeckAndQuestionId(Deck deck, Long questionId);
+    Optional<Question> findByDeckFrnAndQuestionFrn(String deckFrn, String questionFrn);
 
-    @Modifying
     @Transactional
-    void deleteAllByDeck(Deck deck);
+    void deleteAllByDeckFrn(String deckFrn);
 }
--- src/main/java/com/flashdash/repository/UserRepository.java
+++ src/main/java/com/flashdash/repository/UserRepository.java
@@ -2,12 +2,28 @@ package com.flashdash.repository;
 
 import com.flashdash.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
     Optional<User> findByActivationToken(String activationToken);
     List<User> findByDailyNotificationsTrue();
+    @Query(value = "SELECT JSON_UNQUOTE(JSON_EXTRACT(u.friends_frn, '$')) FROM users u WHERE u.user_frn = :userFrn", nativeQuery = true)
+    List<String> findFriendsByUserFrn(String userFrn);
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE users SET friends_frn = JSON_ARRAY_APPEND(friends_frn, '$', :friendFrn) WHERE user_frn = :userFrn", nativeQuery = true)
+    void addFriend(String userFrn, String friendFrn);
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE users SET friends_frn = JSON_REMOVE(friends_frn, JSON_UNQUOTE(JSON_SEARCH(friends_frn, 'one', :friendFrn))) WHERE user_frn = :userFrn", nativeQuery = true)
+    void removeFriend(String userFrn, String friendFrn);
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE users SET friends_frn = '[]' WHERE user_frn = :userFrn", nativeQuery = true)
+    void removeAllFriends(String userFrn);
 }
