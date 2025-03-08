de4f95aaa5c37bcec210a7cc2874932c96cda15c
P4r1nc3 <konrad.tupta@gmail.com>
2025-03-08 09:29:05 +0100

    Created AuthenticatedUserProvider

---
+++ src/main/java/com/flashdash/utils/AuthenticatedUserProvider.java
@@ -0,0 +1,22 @@
+package com.flashdash.utils;
+
+import com.flashdash.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticatedUserProvider {
+
+    public String getUserFrn() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            User user = (User) authentication.getPrincipal();
+            return user.getUserFrn();
+        }
+
+        return null;
+    }
+}
