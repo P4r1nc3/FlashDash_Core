9c8c631f06f521933909958f288c5a9eb5016d27
tupta <konrad.tupta@here.com>
2025-03-17 08:04:11 +0100

    Removed logic from FriendController

--- src/main/java/com/flashdash/core/controller/FriendController.java
+++ src/main/java/com/flashdash/core/controller/FriendController.java
@@ -39,34 +39,6 @@ public class FriendController {
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/invite")
-    public ResponseEntity<Void> sendFriendInvitation(@RequestParam String recipientEmail) {
-        String userFrn = getAuthenticatedUser();
-        friendService.sendFriendInvitation(userFrn, recipientEmail);
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/invitations/received")
-    public ResponseEntity<List<FriendInvitationResponseReceived>> getReceivedInvitations() {
-        String userFrn = getAuthenticatedUser();
-        List<FriendInvitation> invitations = friendService.getReceivedFriendInvitations(userFrn);
-        return ResponseEntity.ok(entityToResponseMapper.mapToReceivedResponse(invitations));
-    }
-
-    @GetMapping("/invitations/sent")
-    public ResponseEntity<List<FriendInvitationResponseSent>> getSentInvitations() {
-        String userFrn = getAuthenticatedUser();
-        List<FriendInvitation> invitations = friendService.getSentFriendInvitations(userFrn);
-        return ResponseEntity.ok(entityToResponseMapper.mapToSentResponse(invitations));
-    }
-
-    @PutMapping("/invitations/{invitationFrn}")
-    public ResponseEntity<Void> respondToInvitation(@PathVariable String invitationFrn, @RequestParam String status) {
-        String userFrn = getAuthenticatedUser();
-        friendService.respondToFriendInvitation(invitationFrn, userFrn, status);
-        return ResponseEntity.ok().build();
-    }
-
     private String getAuthenticatedUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = (User) authentication.getPrincipal();
